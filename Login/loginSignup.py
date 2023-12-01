ba92612e4c5df9510753067357c4bbd5e40ad500
kevinez2 <kevinez2@illinois.edu>
2023-12-01 05:08:47 -0600

    Added login blueprint and uid for session

--- Login/loginSignup.py
+++ Login/loginSignup.py
@@ -1,14 +1,16 @@
-from app import app, getPool, request, sqlalchemy, flash, redirect, session, url_for, render_template, getconn
-
-@app.route('/', methods=['POST'])
+from flask import Blueprint
+from app import app, request, sqlalchemy, flash, redirect, session, url_for, render_template, pool
+login = Blueprint('login', __name__, url_prefix='/login')
+@login.route('/', methods=['POST'])
 def login():
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
         query = f"SELECT count(*) FROM Users WHERE username='{username}' AND password='{password}'"
         #Verify username & password match in Database
-        with getPool(getconn()).connect() as db_conn:
+        with pool.connect() as db_conn:
             results = db_conn.execute(sqlalchemy.text(query)).fetchall()
+            uid = db_conn.execute(sqlalchemy.text(f"select uid from Users where username='{username}' and password='{password}'")).fetchall()[0][0]
 
         #Redirect back to login if no user found
         if results[0][0] == 0:
@@ -17,13 +19,14 @@ def login():
 
         #Using sessions so that user can later have personalized page
         session['username'] = username
+        session['uid'] = uid
 
         return redirect(url_for('gamesearch'))
     
     return render_template("login.html")
 
 
-@app.route('/signup', methods=['POST'])
+@login.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         # Verify username is unique and add user to the database
@@ -33,7 +36,7 @@ def signup():
 
         # Adding user by calling stored procedure
         try:
-            with getPool(getconn).connect() as db_conn:
+            with pool.connect() as db_conn:
                 numUsers = db_conn.execute(sqlalchemy.text("select max(uid) from Users")).fetchall()[0][0] + 1
                 db_conn.execute(sqlalchemy.text(f"insert into Users (uid, username, password) values ({numUsers}, '{username}', '{password}')"))
                 # commit transaction (SQLAlchemy v2.X.X is commit as you go)
@@ -44,6 +47,7 @@ def signup():
         
         #Adding username to session
         session['username'] = username
+        session['uid'] = numUsers
 
         return redirect(url_for('gamesearch'))
     
