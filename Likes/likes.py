1ff6dbe10d5e381a108dc0f6c59ff4e3cdcdae36
TZ-zzz <tangzhen1027@gmail.com>
2023-12-05 16:49:26 -0600

    add try except in likes

--- Likes/likes.py
+++ Likes/likes.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, flash, redirect, render_template, request, url_for, session
 from db import pool
+import pymysql
 
 likes_bp = Blueprint('likes', __name__, url_prefix='/likes')
 @likes_bp.route('/', methods=['GET', 'POST'])
@@ -21,17 +22,20 @@ def like_game():
     cursor.execute("SELECT gid from Likes where uid = %s", (userid,))
     gids = list(cursor.fetchall())
 
-    if gid == ():
-        flash('Game does not exist')
-        return redirect(url_for('games.game', methods=["GET"]))
-    elif gid in gids:
-        flash('Game already liked')
-        return redirect(url_for('games.game', methods=["GET"]))
-    else:
+    # if gid == ():
+    #     flash('Game does not exist')
+    #     return redirect(url_for('games.game', methods=["GET"]))
+    # elif gid in gids:
+    #     flash('Game already liked')
+    #     return redirect(url_for('games.game', methods=["GET"]))
+    # else:
+    try:
         cursor.execute("INSERT INTO Likes (uid, gid) VALUES (%s, %s)", (userid, gid))
         connection.commit()
         flash('Successfully added game')
-        return redirect(url_for('games.game'))
+    except pymysql.err.IntegrityError as e:
+        flash('Game already liked')
+    return redirect(url_for('games.game'))
 
 @likes_bp.route('/remove', methods=["POST"])
 def remove_game():
