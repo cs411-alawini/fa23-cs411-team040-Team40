7a8e685b70854d3600d74fbb7b4b3080d11354b1
TZ-zzz <tangzhen1027@gmail.com>
2023-12-05 16:42:03 -0600

    fix recommends.py

--- Recommends/recommends.py
+++ Recommends/recommends.py
@@ -12,26 +12,28 @@ def recommend_games():
     connection = pool.raw_connection()
     cursor = connection.cursor()
 
-    # Fetch the games liked by the user
-    cursor.execute("SELECT gid FROM Likes WHERE uid = %s", (userid,))
-    user_liked_games = [row[0] for row in cursor.fetchall()]
+    # # Fetch the games liked by the user
+    # cursor.execute("SELECT gid FROM Likes WHERE uid = %s", (userid,))
+    # user_liked_games = [row[0] for row in cursor.fetchall()]
 
-    # Fetch the games liked by the user's friends
-    friends_liked_games = []
-    cursor.execute("SELECT friendid FROM Friends WHERE uid = %s", (userid,))
-    friend_ids = [row[0] for row in cursor.fetchall()]
+    # # Fetch the games liked by the user's friends
+    # friends_liked_games = []
+    # cursor.execute("SELECT friendid FROM Friends WHERE uid = %s", (userid,))
+    # friend_ids = [row[0] for row in cursor.fetchall()]
 
-    for friend_id in friend_ids:
-        cursor.execute("SELECT gid FROM Likes WHERE uid = %s", (friend_id,))
-        friends_liked_games.extend([row[0] for row in cursor.fetchall()])
+    # for friend_id in friend_ids:
+    #     cursor.execute("SELECT gid FROM Likes WHERE uid = %s", (friend_id,))
+    #     friends_liked_games.extend([row[0] for row in cursor.fetchall()])
 
-    # Combine user's and friends' liked games and remove duplicates
-    all_liked_games = set(user_liked_games + friends_liked_games)
+    # # Combine user's and friends' liked games and remove duplicates
+    # all_liked_games = set(user_liked_games + friends_liked_games)
 
-    # Fetch game details based on the liked game IDs
-    cursor.execute(
-        "SELECT gid, title, link, introduction FROM Games WHERE gid IN %s",
-        (tuple(all_liked_games),)
-    )
-    recommended_games = list(cursor.fetchall())
+    # # Fetch game details based on the liked game IDs
+    # cursor.execute(
+    #     "SELECT gid, title, link, introduction FROM Games WHERE gid IN %s",
+    #     (tuple(all_liked_games),)
+    # )
+    cursor.callproc('Game_recommender', (userid,))
+    recommended_games = cursor.fetchall()
+    connection.commit()
     return render_template('games.html', games_list = recommended_games)
